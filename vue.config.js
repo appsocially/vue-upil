56e41c2a92f36f6d5887239cd863dcd3a3688818
Jeremy <jeremy@wallslide.com>
2020-06-05 15:29:06 +0900

    Cleanup code and prepare to publish npm

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,8 @@
 // vue.config.js
 const webpack = require('webpack')
-// const path = require('path')
 
 module.exports = {
   configureWebpack: {
-    // resolve: {
-    //   alias: {
-    //     core: path.resolve(__dirname, '../core')
-    //   }
-    // },
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1
