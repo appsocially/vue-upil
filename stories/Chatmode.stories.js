98f71a9e080a14105fe4d9ee0309202e920a30cf
Jeremy <jeremy@wallslide.com>
2020-07-02 12:43:14 +0900

    Add variable type-validation example

--- stories/Chatmode.stories.js
+++ stories/Chatmode.stories.js
@@ -1,6 +1,16 @@
 import ChatMode from '@/components/ChatMode'
 import { UPILCore } from '@appsocially/userpil-core'
 import TruffleLogo from './truffle_logo.png'
+import { email } from 'vee-validate/dist/rules'
+
+const emailValidationRules = [
+  (value) => (value && value.length > 0 ? true : 'Required'),
+  (value) => (email.validate(value) ? true : 'Invalid email address'),
+]
+
+const types = {
+  email: emailValidationRules,
+}
 
 export default { title: 'Chatmode' }
 
@@ -25,6 +35,36 @@ export const basic = () => {
       return {
         upil,
         TruffleLogo,
+      }
+    },
+    mounted() {
+      this.upil.startRaw(simpleTemplate)
+    },
+  }
+}
+
+export const emailValidation = () => {
+  const simpleTemplate = `
+    DIALOG icecream
+      TEMPLATE 
+      "What's your email?"
+      >>email:email
+      /TEMPLATE
+      TEMPLATE "Thank you for your email (\${email})"
+    /DIALOG
+    RUN icecream
+  `
+  const upil = new UPILCore()
+  return {
+    components: {
+      ChatMode,
+    },
+    template: ` <ChatMode :upil="upil" key="Template" :avatar="TruffleLogo" :types="types"/>`,
+    data() {
+      return {
+        upil,
+        TruffleLogo,
+        types,
       }
     },
     mounted() {
