0ebaca23e0ffa6fc463fa58fee400278ddc2ff9d
Jeremy <jeremy@wallslide.com>
2020-07-23 16:03:35 +0900

    Add longer examples

--- stories/Formmode.stories.js
+++ stories/Formmode.stories.js
@@ -43,3 +43,71 @@ export const basic = () => {
     },
   }
 }
+
+export const longer = () => {
+  const longerTemplate = `
+  DIALOG mainDialog
+  TEMPLATE "Welcome to our job application bot!"
+  TEMPLATE
+    {
+      formText: "First name"
+    }
+    "What's your first name?"
+    >>firstname
+  /TEMPLATE
+  TEMPLATE
+    {
+      formText: "Last name"
+    }
+    "What's your last name?"
+    >>lastname
+  /TEMPLATE
+  SELECT
+    {
+      formText: "Target job"
+    }
+    "\${firstname} \${lastname}, what job do you want to apply for?"
+    -"Janitor"
+    -"Chef"
+    >>selectedJob
+  /SELECT
+  IF selectedJob == "Janitor"
+    TEMPLATE
+      {
+        formText: "Favorite vacuum"
+      }
+      "What is your favorite brand of vacuum?"
+      >>favoriteVacuum
+    /TEMPLATE
+    ELSE
+      TEMPLATE
+      {
+        formText: "Favorite dish"
+      }
+      "What is your favorite dish to make?"
+      >>favoriteDish
+    /TEMPLATE
+  /IF
+  TEMPLATE "Thank you for your application!"
+/DIALOG
+RUN mainDialog
+  `
+  const upil = new UPILCore()
+  return {
+    components: {
+      FormMode,
+    },
+    template: ` <FormMode :upil="upil" />`,
+    data() {
+      return {
+        upil,
+      }
+    },
+    mounted() {
+      this.upil.startRaw(longerTemplate, {
+        mode: 'form',
+        resetOnInputUpdate: true,
+      })
+    },
+  }
+}
