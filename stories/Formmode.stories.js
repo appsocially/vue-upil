1e3909704107ef37c7501468e6b4832d902aa06e
Jeremy <jeremy@wallslide.com>
2020-07-24 10:39:22 +0900

    Add dual-formmode story to debug sharing issue

--- stories/Formmode.stories.js
+++ stories/Formmode.stories.js
@@ -111,3 +111,134 @@ RUN mainDialog
     },
   }
 }
+
+export const doubleFormMode = () => {
+  const longerTemplate1 = `
+  DIALOG mainDialog
+  TEMPLATE "Welcome to our job application bot!"
+  TEMPLATE
+    {
+      formText: "First name1"
+    }
+    "What's your first name?"
+    >>firstname
+  /TEMPLATE
+  TEMPLATE
+    {
+      formText: "Last name1"
+    }
+    "What's your last name?"
+    >>lastname
+  /TEMPLATE
+  SELECT
+    {
+      formText: "Target job1"
+    }
+    "\${firstname} \${lastname}, what job do you want to apply for?"
+    -"Janitor"
+    -"Chef"
+    >>selectedJob
+  /SELECT
+  IF selectedJob == "Janitor"
+    TEMPLATE
+      {
+        formText: "Favorite vacuum1"
+      }
+      "What is your favorite brand of vacuum?"
+      >>favoriteVacuum
+    /TEMPLATE
+    ELSE
+      TEMPLATE
+      {
+        formText: "Favorite dish1"
+      }
+      "What is your favorite dish to make?"
+      >>favoriteDish
+    /TEMPLATE
+  /IF
+  TEMPLATE "Thank you for your application!"
+/DIALOG
+RUN mainDialog
+  `
+  const longerTemplate2 = `
+  DIALOG mainDialog
+  TEMPLATE "Welcome to our job application bot!"
+  TEMPLATE
+    {
+      formText: "First name2"
+    }
+    "What's your first name?"
+    >>firstname
+  /TEMPLATE
+  TEMPLATE
+    {
+      formText: "Last name2"
+    }
+    "What's your last name?"
+    >>lastname
+  /TEMPLATE
+  SELECT
+    {
+      formText: "Target job2"
+    }
+    "\${firstname} \${lastname}, what job do you want to apply for?"
+    -"Janitor"
+    -"Chef"
+    >>selectedJob
+  /SELECT
+  IF selectedJob == "Janitor"
+    TEMPLATE
+      {
+        formText: "Favorite vacuum2"
+      }
+      "What is your favorite brand of vacuum?"
+      >>favoriteVacuum
+    /TEMPLATE
+    ELSE
+      TEMPLATE
+      {
+        formText: "Favorite dish2"
+      }
+      "What is your favorite dish to make?"
+      >>favoriteDish
+    /TEMPLATE
+  /IF
+  TEMPLATE "Thank you for your application!"
+/DIALOG
+RUN mainDialog
+  `
+  const upil1 = new UPILCore()
+  const upil2 = new UPILCore()
+  return {
+    components: {
+      FormMode,
+    },
+    template: ` 
+    <div>
+      <v-sheet color="orange" class="pa-3">
+        <FormMode :upil="upil1" />
+      </v-sheet>
+    <br/><br/>
+      <v-sheet color="green" class="pa-3">
+        <FormMode :upil="upil2" />
+      </v-sheet>
+    </div>
+    `,
+    data() {
+      return {
+        upil1,
+        upil2,
+      }
+    },
+    mounted() {
+      this.upil1.startRaw(longerTemplate1, {
+        mode: 'form',
+        resetOnInputUpdate: true,
+      })
+      this.upil2.startRaw(longerTemplate2, {
+        mode: 'form',
+        resetOnInputUpdate: true,
+      })
+    },
+  }
+}
