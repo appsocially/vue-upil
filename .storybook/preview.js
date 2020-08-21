a871ff301555181158099edbfc57da7e68e42293
Jeremy <jeremy@wallslide.com>
2020-08-21 10:26:45 +0900

    Add vuetify theme override

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,11 +6,28 @@ import '@mdi/font/css/materialdesignicons.css'
 Vue.use(Vuetify)
 
 export const decorators = [
-  () => ({
-    template: `
-    <v-app><story/></v-app>
-    `,
-    vuetify: new Vuetify()
-  })
+  (story, {args}) => {
+    const vuetifyObj = args.vuetify || {
+      theme: {
+        themes: {
+            light: {
+              primary: '#1976D2',
+              secondary: '#424242',
+              accent: '#82B1FF',
+              error: '#FF5252',
+              info: '#2196F3',
+              success: '#4CAF50',
+              warning: '#FFC107',
+            }
+          }
+        }
+      }
+    return {
+      template: `
+      <v-app><story/></v-app>
+      `,
+      vuetify: new Vuetify(vuetifyObj)
+    } 
+  }
 ]
 
