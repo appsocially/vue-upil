258deff9f6422cd653292eae2188679fc474d75b
Jeremy <jeremy@wallslide.com>
2020-09-11 09:54:22 +0900

    Create formmode time-input widget

---
+++ src/components/FormMode/time-input/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <v-row no-gutters class="mx-1" justify="start" align="center">
+    <v-col cols="auto">
+      <v-select
+        class="time-input"
+        :placeholder="hoursSelectLabel"
+        :items="hoursItems"
+        v-model="hoursModel"
+      />
+    </v-col>
+    <v-col cols="auto" class="ma-1 text-h5">
+      {{ unitSeparator }}
+    </v-col>
+    <v-col cols="auto">
+      <v-select
+        class="time-input"
+        :placeholder="minutesSelectlabel"
+        :items="minutesItems"
+        v-model="minutesModel"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { VRow, VCol, VSelect } from 'vuetify/lib'
+import { symbols } from '@appsocially/userpil-core'
+import widgeti18nMixin from '@/components/widgeti18nMixin'
+
+export default {
+  mixins: [widgeti18nMixin],
+  components: {
+    VRow,
+    VCol,
+    VSelect,
+  },
+  props: {
+    node: {
+      type: Object,
+      required: true,
+    },
+    state: {
+      type: Object,
+      required: true,
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    locale: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      isValid: this.rules.length === 0,
+      tempHours: null,
+      tempMinutes: null,
+      hoursRaw: Array.from(Array(24).keys()),
+      minutesRaw: Array.from(Array(60).keys()),
+    }
+  },
+  computed: {
+    hoursItems() {
+      return this.hoursRaw.map((i) => ({
+        text: `${i}${this.unitHour}`.padStart(2 + this.unitHour.length, 0),
+        value: i,
+      }))
+    },
+    minutesItems() {
+      return this.minutesRaw.map((i) => ({
+        text: `${i}${this.unitMinute}`.padStart(2 + this.unitMinute.length, 0),
+        value: i,
+      }))
+    },
+    inputName() {
+      return this.node.input.name
+    },
+    stateInputValue() {
+      const inputValue = this.state[this.inputName]
+      return inputValue === symbols.UNRESOLVED ? null : inputValue
+    },
+    hoursModel: {
+      get() {
+        return this.tempHours ? this.tempHours : this.stateHours
+      },
+      set(value) {
+        this.tempHours = value
+        this.checkSubmit()
+      },
+    },
+    minutesModel: {
+      get() {
+        return this.tempMinutes ? this.tempMinutes : this.stateMinutes
+      },
+      set(value) {
+        this.tempMinutes = value
+        this.checkSubmit()
+      },
+    },
+    stateHours() {
+      return this.stateInputValue ? this.stateInputValue.hours : null
+    },
+    stateMinutes() {
+      return this.stateInputValue ? this.stateInputValue.minutes : null
+    },
+    hoursSelectLabel() {
+      return this.localeArgLookup('hoursSelectLabel') || 'hour'
+    },
+    minutesSelectlabel() {
+      return this.localeArgLookup('minutesSelectlabel') || 'minutes'
+    },
+    unitHour() {
+      return this.localeArgLookup('unitHour') || ''
+    },
+    unitMinute() {
+      return this.localeArgLookup('unitMinute') || ''
+    },
+    unitSeparator() {
+      return this.localeArgLookup('unitSeparator') || ':'
+    },
+  },
+  watch: {
+    stateInputValue: {
+      immediate: true,
+      handler(stateInputValue) {
+        if (stateInputValue) {
+          this.resetTempValues()
+        }
+      },
+    },
+  },
+  methods: {
+    checkSubmit() {
+      if (this.hoursModel && this.minutesModel) {
+        this.$emit('consume', {
+          event: this.node.event,
+          value: { hours: this.hoursModel, minutes: this.minutesModel },
+        })
+      }
+    },
+    resetTempValues() {
+      this.tempHours = null
+      this.tempMinutes = null
+    },
+  },
+}
+</script>
+
+<style scoped>
+.time-input {
+  max-width: 100px;
+}
+</style>
---
+++ stories/TimeInput.stories.js
@@ -0,0 +1,163 @@
+import TruffleLogo from './truffle_logo.png'
+import { parse } from 'date-fns'
+import FormMode from '@/components/FormMode'
+import WizardMode from '@/components/WizardMode'
+import ChatMode from '@/components/ChatMode'
+import { UPILCore } from '@appsocially/userpil-core'
+import { setupListeners } from '@/utils'
+import { formatTextbox as formatDateTimeString } from '@/components/FormMode/date-time/utils'
+import { isDate } from 'date-fns'
+
+const transformTextVariables = ({ value, locale }) => {
+  if (isDate(value)) {
+    return formatDateTimeString(value, locale)
+  } else {
+    return value
+  }
+}
+
+const transformReplyVariables = ({
+  node: {
+    event: { value },
+  },
+  locale,
+}) => {
+  if (isDate(value)) {
+    return formatDateTimeString(value, locale)
+  } else {
+    return value
+  }
+}
+
+export default {
+  title: 'Widgets/Time-Input Widget',
+  args: { mode: 'FormMode', listeners: {}, locale: 'en' },
+  argTypes: {
+    mode: {
+      control: {
+        type: 'select',
+        options: ['FormMode', 'WizardMode', 'ChatMode'],
+      },
+    },
+    locale: {
+      control: {
+        type: 'radio',
+        options: ['en', 'ja'],
+      },
+    },
+    listeners: {
+      type: 'object',
+    },
+  },
+}
+
+const dateTimeTemplate = (args) => {
+  const birthdayTemplate = `
+  DIALOG meetingTime
+    TEMPLATE time-input
+    {
+      formText: "Meetings start time",
+      i18n: {
+        ja: {
+          formText: "ミーティング時間の開始",
+          text: "ミーティング時間はいつから始まる？"
+        }
+      }
+    }
+    "When do your meetings start?"
+    >>meetingsStart
+    /TEMPLATE
+    TEMPLATE time-input
+    {
+      formText: "Meetings end time",
+      i18n: {
+        ja: {
+          formText: "ミーティング時間のお終い",
+          text: "ミーティング時間はいつまで続く？"
+        }
+      }
+    }
+    "When do your meetings end?"
+    >>meetingsEnd
+    /TEMPLATE
+    TEMPLATE 
+    {
+      i18n: {
+        ja: {
+          text: "ミーティング時間が\${meetingsStart}から\${meetingsEnd}まではけっこう長いんでしょう～"
+        }
+      }
+    }
+    "From \${meetingsStart} to \${meetingsEnd} is a pretty long time don't you think?" 
+    /TEMPLATE
+  /DIALOG
+  RUN meetingTime
+  `
+
+  return {
+    props: Object.keys(args),
+    components: {
+      FormMode,
+      WizardMode,
+      ChatMode,
+    },
+    template: `<component v-if="upil" :locale="locale" :is="mode" :upil="upil" :key="mode" :avatar="TruffleLogo" :transformTextVariables="transformTextVariables" :transformReplyVariables="transformReplyVariables"/>`,
+    data() {
+      return {
+        upil: null,
+        TruffleLogo,
+      }
+    },
+    methods: {
+      startUpil() {
+        this.upil = new UPILCore()
+        setupListeners({ upil: this.upil, listeners: this.listeners })
+
+        if (this.mode === 'ChatMode') {
+          this.upil.startRaw(birthdayTemplate, {})
+        } else {
+          this.upil.startRaw(birthdayTemplate, {
+            mode: 'form',
+            resetOnInputUpdate: true,
+          })
+        }
+      },
+      transformTextVariables,
+      transformReplyVariables,
+    },
+    mounted() {
+      this.startUpil()
+    },
+    watch: {
+      mode() {
+        this.startUpil()
+      },
+    },
+  }
+}
+
+export const DateTimeEmpty = dateTimeTemplate.bind({})
+// DateFormMode.args = { mode: 'FormMode' }
+
+export const DateTimePreLoaded = dateTimeTemplate.bind({})
+DateTimePreLoaded.args = {
+  mode: 'FormMode',
+  listeners: {
+    'preload-input': async () => {
+      return {
+        partyDateTime: parse('2019-08-05:15:5', 'yyyy-MM-dd:H:m', new Date()),
+      }
+    },
+  },
+}
+
+export const DateTimeChat = dateTimeTemplate.bind({})
+DateTimeChat.args = {
+  mode: 'ChatMode',
+}
+
+export const DateTimeChatJa = dateTimeTemplate.bind({})
+DateTimeChatJa.args = {
+  mode: 'ChatMode',
+  locale: 'ja',
+}
