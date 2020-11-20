1139cebb65fceca3eba610130b4a7a9a2a31adfc
Duong Van Lam <lamdv@tadi.io>
2020-11-20 11:57:56 +0700

    Add phone validation

--- stories/Validation.stories.js
+++ stories/Validation.stories.js
@@ -1,10 +1,11 @@
-import TruffleLogo from './truffle_logo.png'
+import ChatMode from '@/components/ChatMode'
 import FormMode from '@/components/FormMode'
 import WizardMode from '@/components/WizardMode'
-import ChatMode from '@/components/ChatMode'
-import { UPILCore } from '@appsocially/userpil-core'
 import { setupListeners } from '@/utils'
+import { UPILCore } from '@appsocially/userpil-core'
+import isMobilePhone from 'validator/lib/isMobilePhone'
 import { email } from 'vee-validate/dist/rules'
+import TruffleLogo from './truffle_logo.png'
 
 const requiredMessageChooser = (locale) => {
   switch (locale) {
@@ -24,6 +25,15 @@ const invalidEmailAddressChooser = (locale) => {
   }
 }
 
+const invalidPhoneNumberChooser = (locale) => {
+  switch (locale) {
+    case 'ja':
+      return '有効な電話番号ではありません'
+    default:
+      return 'Invalid phone number'
+  }
+}
+
 const emailValidationRules = (locale) => [
   (value) =>
     value && value.length > 0 ? true : requiredMessageChooser(locale),
@@ -31,8 +41,16 @@ const emailValidationRules = (locale) => [
     email.validate(value) ? true : invalidEmailAddressChooser(locale),
 ]
 
+const phoneValidationRules = (locale) => [
+  (value) =>
+    value && value.length > 0 ? true : requiredMessageChooser(locale),
+  (value) =>
+    isMobilePhone(value) ? true : invalidPhoneNumberChooser(locale),
+]
+
 const types = {
   email: emailValidationRules,
+  phone: phoneValidationRules,
 }
 
 export default {
@@ -137,6 +155,86 @@ const emailValidationTemplate = (args) => {
   }
 }
 
+const phoneValidationTemplate = (args) => {
+  const birthdayTemplate = `
+  DIALOG getPhone
+    TEMPLATE
+    {
+      formText: "Phone",
+      inputType: "phone",
+      i18n: {
+        ja: {
+          formText: "電話番号",
+          text: "電話番号教えてください。"
+        }
+      }
+    }
+    "Please enter your phone address"
+    >>customerPhone:phone
+    /TEMPLATE
+    TEMPLATE 
+    {
+      i18n: {
+        ja: {
+          text: "\${customerPhone}がかっこいい！"
+        }
+      }
+    }
+    "\${customerPhone} is such a cool phone number!"
+    /TEMPLATE
+    /DIALOG
+  RUN getPhone
+  `
+
+  return {
+    props: Object.keys(args),
+    components: {
+      FormMode,
+      WizardMode,
+      ChatMode,
+    },
+    template: `<component v-if="upil" :is="mode" :upil="upil" :key="mode" :avatar="TruffleLogo" :types="types" :locale="locale" :i18n="i18n"/>`,
+    data() {
+      return {
+        upil: null,
+        TruffleLogo,
+        types,
+        i18n: {
+          ja: {
+            missingValue: '未記入',
+            templateInputPlaceholder: '入力してください',
+            selectInputPlaceholder: '選んでください',
+            multiSelectInputPlaceholder: '選んでください',
+          },
+        },
+      }
+    },
+    methods: {
+      startUpil() {
+        this.upil = new UPILCore()
+        setupListeners({ upil: this.upil, listeners: this.listeners })
+
+        if (this.mode === 'ChatMode') {
+          this.upil.startRaw(birthdayTemplate, {})
+        } else {
+          this.upil.startRaw(birthdayTemplate, {
+            mode: 'form',
+            resetOnInputUpdate: true,
+          })
+        }
+      },
+    },
+    mounted() {
+      this.startUpil()
+    },
+    watch: {
+      mode() {
+        this.startUpil()
+      },
+    },
+  }
+}
+
 export const EmailValidationForm = emailValidationTemplate.bind({})
 
 export const EmailValidationChat = emailValidationTemplate.bind({})
@@ -146,6 +244,18 @@ EmailValidationChat.args = {
 
 export const EmailValidationChatJa = emailValidationTemplate.bind({})
 EmailValidationChatJa.args = {
+  mode: 'ChatMode',
+  locale: 'ja',
+}
+
+
+export const PhoneValidationChat = phoneValidationTemplate.bind({})
+PhoneValidationChat.args = {
+  mode: 'ChatMode',
+}
+
+export const PhoneValidationChatJa = phoneValidationTemplate.bind({})
+PhoneValidationChatJa.args = {
   mode: 'ChatMode',
   locale: 'ja',
 }
